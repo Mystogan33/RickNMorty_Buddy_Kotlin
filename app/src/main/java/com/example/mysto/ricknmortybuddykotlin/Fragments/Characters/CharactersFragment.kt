429e6e19fbfec9832dddf8d69ab5193abcd6284a
Valentin GAY <mystogan40@gmail.com>
2019-04-15 02:53:12 +0200

    Add custom Rick'N'Morty fonts :)

--- app/src/main/java/com/example/mysto/ricknmortybuddykotlin/Fragments/Characters/CharactersFragment.kt
+++ app/src/main/java/com/example/mysto/ricknmortybuddykotlin/Fragments/Characters/CharactersFragment.kt
@@ -2,10 +2,12 @@ package com.example.mysto.ricknmortybuddykotlin.Fragments.Characters
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
